d21a996bb31beaa93749813b4e3a6a4302d24aec
Andros Fenollosa <andros@fenollosa.email>
2025-09-23 16:53:11 +0200

    Updated endpoints

--- app/groups/urls.py
+++ app/groups/urls.py
@@ -3,6 +3,6 @@ from .views import GroupsView, GroupMembersView, GroupMessagesView
 
 urlpatterns = [
     path("", GroupsView.as_view(), name="groups-list"),
-    path("<int:group_id>/", GroupMessagesView.as_view(), name="group-messages"),
-    path("<int:group_id>/members/", GroupMembersView.as_view(), name="group-members"),
+    path("<str:group_name>/", GroupMessagesView.as_view(), name="group-messages"),
+    path("<str:group_name>/members/", GroupMembersView.as_view(), name="group-members"),
 ]
