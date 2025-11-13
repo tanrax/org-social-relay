5df132349c374f6a9acce62089706d9db38de29a
Andros Fenollosa <andros@fenollosa.email>
2025-11-13 17:36:55 +0100

    Added render

---
+++ app/reactions/renderers.py
@@ -0,0 +1,30 @@
+from rest_framework.renderers import JSONRenderer
+import json
+
+
+class UTF8JSONRenderer(JSONRenderer):
+    """
+    JSON renderer that preserves UTF-8 characters (like emojis) instead of escaping them.
+    """
+    charset = 'utf-8'
+    
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        if data is None:
+            return bytes()
+        
+        renderer_context = renderer_context or {}
+        indent = self.get_indent(accepted_media_type, renderer_context)
+        
+        if indent is None:
+            separators = (',', ':')
+        else:
+            separators = (',', ': ')
+        
+        ret = json.dumps(
+            data,
+            ensure_ascii=False,
+            indent=indent,
+            separators=separators
+        )
+        
+        return ret.encode('utf-8')
